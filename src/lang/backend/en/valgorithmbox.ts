c0e00b24abb9121654d873d59dc13b260b8ce730
Nicole_zcr <3380782523@qq.com>
2026-03-29 21:48:25 +0800

    Init main again

--- src/lang/backend/en/valgorithmbox.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    id: 'id',
-    name: 'name',
-    avatar: 'avatar',
-    content: 'content',
-    create_time: 'create_time',
-    update_time: 'update_time',
-    'quick Search Fields': 'id',
-}
--- src/lang/backend/zh-cn/firmware.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    firmware: '固件管理',
-    'firmware/version': '固件版本',
-    'firmware/upgrade': '升级任务',
-}
